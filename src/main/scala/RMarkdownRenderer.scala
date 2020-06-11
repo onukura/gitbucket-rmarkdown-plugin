4b4ad882b5e23fd79c01fd0a17e1883670be1869
onukura <y.bibos.ishikawa@gmail.com>
2020-06-12 02:11:19 +0900

    add mathjax support

--- src/main/scala/RMarkdownRenderer.scala
+++ src/main/scala/RMarkdownRenderer.scala
@@ -1,7 +1,7 @@
 import gitbucket.core.controller.Context
-import gitbucket.core.view.Markdown
 import gitbucket.core.plugin.{RenderRequest, Renderer}
 import gitbucket.core.service.RepositoryService.RepositoryInfo
+import gitbucket.core.view.helpers.markdown
 import play.twirl.api.Html
 
 class RMarkdownRenderer extends Renderer {
@@ -20,21 +20,26 @@ class RMarkdownRenderer extends Renderer {
               enableRefsLink: Boolean,
               enableAnchor: Boolean)(implicit context: Context): String = {
 
-    val markdown = fileContent
+    val content = fileContent
       .replaceAll("^---\\s*\ntitle:(.*)","# $1\n\n```")
       .replaceAll("\n---\\s*\n", "\n```\n")
 
-    Markdown.toHtml(
-      markdown = markdown,
-      repository = repository,
-      branch = branch,
-      enableWikiLink = enableWikiLink,
-      enableRefsLink = enableRefsLink,
-      enableAnchor = enableAnchor,
-      enableLineBreaks = false,
-      enableTaskList = true,
-      hasWritePermission = false
-    )(context)
+    val rendered = markdown(content, repository, branch, enableWikiLink, enableRefsLink, enableLineBreaks = true)
+
+    val delim = "$"
+
+    s"""<script>
+       MathJax = {
+         tex: {
+           inlineMath: [['$delim', '$delim'], ['\\(', '\\)']]
+         }
+       };
+       </script>
+       <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
+       <script type="text/javascript" id="MathJax-script" async
+         src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js">
+       </script>$rendered"""
+
   }
 
   def shutdown(): Unit = {
